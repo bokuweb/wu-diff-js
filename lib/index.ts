6a756cf4fe45bf1bf611f264b6b9fec163646340
bokuweb <bokuweb12@gmail.com>
2022-04-14 15:43:48 +0900

    0.1.11 (#27)
    * fix
    
    * 0.1.8
    
    * 0.1.11

--- lib/index.ts
+++ lib/index.ts
@@ -10,9 +10,17 @@ export interface DiffResult<T> {
   value: T;
 }
 
+// HACK: Avoid invalid array length
+// https://stackoverflow.com/questions/37421749/is-there-an-item-limit-for-large-arrays-in-javascript#:~:text=In%20Chrome%20maximum%20array%20size%20is%20something%20between%201e9%20and%201e10.&text=And%20crashing%20is%20not%20related,with%20a%20normal%20loop%20too
+const CHUNK_SIZE = 50000000;
+
 const REMOVED = 1;
 const COMMON = 2;
 const ADDED = 3;
+
+const chunkIndex = (ptr: number): number => {
+  return ~~(ptr / CHUNK_SIZE);
+};
 
 function createCommon<T>(A: T[], B: T[], reverse?: boolean) {
   const common = [];
@@ -34,8 +42,9 @@ export default function diff<T>(A: T[], B: T[]): DiffResult<T>[] {
     const result = [];
     let a = M - 1;
     let b = N - 1;
-    let j = routes[current.id];
-    let type = routes[current.id + diffTypesPtrOffset];
+    const idChunkIndex = chunkIndex(current.id);
+    let j = routes[idChunkIndex][current.id % CHUNK_SIZE];
+    let type = types[idChunkIndex][current.id % CHUNK_SIZE];
     while (true) {
       if (!j && !type) break;
       const prev = j;
@@ -50,8 +59,9 @@ export default function diff<T>(A: T[], B: T[]): DiffResult<T>[] {
         a -= 1;
         b -= 1;
       }
-      j = routes[prev];
-      type = routes[prev + diffTypesPtrOffset];
+      const index = chunkIndex(prev);
+      j = routes[index][prev % CHUNK_SIZE];
+      type = types[index][prev % CHUNK_SIZE];
     }
     return result;
   }
@@ -61,14 +71,16 @@ export default function diff<T>(A: T[], B: T[]): DiffResult<T>[] {
     if ((down && down.y === -1) || k === M || (slide && slide.y) > (down && down.y) + 1) {
       const prev = slide.id;
       ptr++;
-      routes[ptr] = prev;
-      routes[ptr + diffTypesPtrOffset] = ADDED;
+      const index = chunkIndex(ptr);
+      routes[index][ptr % CHUNK_SIZE] = prev;
+      types[index][ptr % CHUNK_SIZE] = ADDED;
       return { y: slide.y, id: ptr };
     } else {
       const prev = down.id;
       ptr++;
-      routes[ptr] = prev;
-      routes[ptr + diffTypesPtrOffset] = REMOVED;
+      const index = chunkIndex(ptr);
+      routes[index][ptr % CHUNK_SIZE] = prev;
+      types[index][ptr % CHUNK_SIZE] = REMOVED;
       return { y: down.y + 1, id: ptr };
     }
   }
@@ -83,8 +95,9 @@ export default function diff<T>(A: T[], B: T[]): DiffResult<T>[] {
       ptr++;
       fp.id = ptr;
       fp.y += 1;
-      routes[ptr] = prev;
-      routes[ptr + diffTypesPtrOffset] = COMMON;
+      const index = chunkIndex(ptr);
+      routes[index][ptr % CHUNK_SIZE] = prev;
+      types[index][ptr % CHUNK_SIZE] = COMMON;
     }
     return fp;
   }
@@ -108,12 +121,13 @@ export default function diff<T>(A: T[], B: T[]): DiffResult<T>[] {
   const offset = N;
   const delta = M - N;
   const size = M + N + 1;
+  const arrSize = M * N + size + 1;
+  const numberOfChunks = Math.ceil(arrSize / CHUNK_SIZE);
+
   let fp = new Array(size).fill({ y: -1 });
-  // INFO: This buffer is used to save memory and improve performance.
-  //       The first half is used to save route and last half is used to save diff type.
-  //       This is because, when I kept new uint8array area to save type, performance worsened.
-  let routes = new Array((M * N + size + 1) * 2);
-  const diffTypesPtrOffset = routes.length / 2;
+
+  let routes: number[][] = [...new Array(numberOfChunks)].map(() => new Array(CHUNK_SIZE));
+  let types: number[][] = [...new Array(numberOfChunks)].map(() => new Array(CHUNK_SIZE));
   let ptr = 0;
   let p = -1;
   while (fp[delta + offset].y < N) {
